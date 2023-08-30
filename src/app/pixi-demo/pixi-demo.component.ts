12c1c02163be92cdd523f9fb784b4f7ea3bae30f
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2023-08-30 22:41:09 +0200

    1

--- src/app/pixi-demo/pixi-demo.component.ts
+++ src/app/pixi-demo/pixi-demo.component.ts
@@ -1,7 +1,5 @@
 import { AfterViewInit, Component, ElementRef, NgZone, ViewChild } from '@angular/core';
-import * as PIXI from 'pixi.js';
-import { Application } from '@pixi/app';
-import { Sprite } from '@pixi/sprite';
+import { Application, Container, Sprite, Texture } from 'pixi.js';
 
 @Component({
   selector: 'app-pixi-demo',
@@ -13,12 +11,29 @@ export class PixiDemoComponent implements AfterViewInit {
   
   constructor(private readonly zone: NgZone) {}
 
-  public ngAfterViewInit(): void {
+  ngAfterViewInit(): void {
     this.zone.runOutsideAngular((): void => {
-      const app: Application = new Application({ view: this.canvas.nativeElement });
-      const sprite: Sprite = Sprite.from('https://avatars.githubusercontent.com/in/2740?s=64&v=4');
-      app.stage.addChild(sprite);
-      app.render();
+      const app = new Application({ background: 'gray', resizeTo: window, view: this.canvas.nativeElement });
+      const container = new Container();
+      app.stage.addChild(container);
+      const texture = Texture.from('https://pixijs.com/assets/bunny.png');
+
+      for (let i = 0; i < 25; i++) {
+        const bunny = new Sprite(texture);
+        bunny.anchor.set(0.5);
+        bunny.x = (i % 5) * 40;
+        bunny.y = Math.floor(i / 5) * 40;
+        container.addChild(bunny);
+      }
+
+      container.x = app.screen.width / 2;
+      container.y = app.screen.height / 2;
+      container.pivot.x = container.width / 2;
+      container.pivot.y = container.height / 2;
+
+      app.ticker.add((delta) => {
+        container.rotation -= 0.01 * delta;
+      });
     });
   }
 }
