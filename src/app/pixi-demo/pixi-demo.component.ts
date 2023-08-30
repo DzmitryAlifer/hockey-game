a04b36aea0ffa043fbe67889de0a180777ae8f71
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2023-08-30 22:36:20 +0200

    1

--- src/app/pixi-demo/pixi-demo.component.ts
+++ src/app/pixi-demo/pixi-demo.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, NgZone } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, NgZone, ViewChild } from '@angular/core';
 import * as PIXI from 'pixi.js';
 import { Application } from '@pixi/app';
 import { Sprite } from '@pixi/sprite';
@@ -6,28 +6,19 @@ import { Sprite } from '@pixi/sprite';
 @Component({
   selector: 'app-pixi-demo',
   standalone: true,
-  template: '<h1>game</h1>',
+  template: '<canvas #canvas></canvas>',
 })
 export class PixiDemoComponent implements AfterViewInit {
-  public boxSize: number = 3;
-  constructor(public elRef: ElementRef, private zone: NgZone) {
-    // The application will create a canvas element for you that you
-    // can then insert into the DOM.
-    console.log(PIXI.VERSION);
-  }
+  @ViewChild('canvas') canvas!: ElementRef;
+  
+  constructor(private readonly zone: NgZone) {}
 
   public ngAfterViewInit(): void {
-    this.zone.runOutsideAngular(
-      (): void => {
-        const app: Application = new Application({
-          //view: this.myCanvas.nativeElement,
-        });
-        this.elRef.nativeElement.appendChild(app.view);
-        console.log('Plugins', app.renderer.plugins);
-        const sprite: Sprite = Sprite.from('https://avatars.githubusercontent.com/in/2740?s=64&v=4');
-        app.stage.addChild(sprite);
-        app.render();
-      }
-    );
+    this.zone.runOutsideAngular((): void => {
+      const app: Application = new Application({ view: this.canvas.nativeElement });
+      const sprite: Sprite = Sprite.from('https://avatars.githubusercontent.com/in/2740?s=64&v=4');
+      app.stage.addChild(sprite);
+      app.render();
+    });
   }
 }
