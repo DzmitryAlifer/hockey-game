9b3253aa46d56a7459b72805a529c8d06113ccc1
DzmitryAlifer <dzmitry.alifer@gmail.com>
2023-08-31 11:30:32 +0200

    1

--- src/app/pixi-demo/pixi-demo.component.ts
+++ src/app/pixi-demo/pixi-demo.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, NgZone, ViewChild } from '@angular/core';
-import { Application, Assets, BaseTexture, BLEND_MODES, Container, Rectangle, Sprite, Texture } from 'pixi.js';
-import { RINK_LENGTH_PX, RINK_WIDTH_PX } from 'src/utils/render';
+import { Application, Assets, BaseTexture, BLEND_MODES, Container, Graphics, Rectangle, Sprite, Texture } from 'pixi.js';
+import { CORNER_SEGMENT_SIZE_PX, PLAYER_SIZE_PX, PUCK_RADIUS_PX, RINK_LENGTH_PX, RINK_WIDTH_PX } from 'src/utils/render';
 import * as PIXI from 'pixi.js';
 
 @Component({
@@ -15,25 +15,69 @@ export class PixiDemoComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     this.zone.runOutsideAngular(async () => {
-      const app = new Application<HTMLCanvasElement>({ background: 'grey', resizeTo: window, resolution: 1 });
-      this.elementRef.nativeElement.appendChild(app.view);
+      const app = this.getApp();
+      const player = getPlayer();
 
-      const backgroundRink = Sprite.from(await Assets.load('../../assets/images/rink.svg'), { multisample: 8 });
-      backgroundRink.height = RINK_WIDTH_PX;
-      backgroundRink.width = RINK_LENGTH_PX;
-      
-      app.stage.addChild(backgroundRink);
-
-      const player = Sprite.from('../../assets/images/jersey_red.png');
+      app.stage.addChild(await getBackgroundRink());
+      app.stage.addChild(getRinkBorder());
       app.stage.addChild(player);
+      app.stage.addChild(getPuck());
       
-      player.anchor.set(0.5);
-      player.x = RINK_LENGTH_PX / 2;
-      player.y = RINK_WIDTH_PX / 2;
-
       app.ticker.add((delta) => {
-        player.rotation += 0.05 * delta;
+        player.rotation += 0.03 * delta;
       });
     });
   }
+
+  getApp(): Application {
+    const app = new Application<HTMLCanvasElement>({ background: 'white', resizeTo: window, antialias: true });
+    this.elementRef.nativeElement.appendChild(app.view);
+
+    return app;
+  }
+}
+
+async function getBackgroundRink(): Promise<Sprite> {
+  const backgroundRinkSprite = Sprite.from(await Assets.load('../../assets/images/rink.jpg'));
+  backgroundRinkSprite.height = RINK_WIDTH_PX;
+  backgroundRinkSprite.width = RINK_LENGTH_PX;
+  backgroundRinkSprite.alpha = 0.05;
+
+  return backgroundRinkSprite;
+}
+
+function getRinkBorder(): Graphics {
+  const rinkBorder = new Graphics();
+  rinkBorder.lineStyle(2, 'green');
+  rinkBorder.drawPolygon(
+    CORNER_SEGMENT_SIZE_PX, 0,
+    RINK_LENGTH_PX - CORNER_SEGMENT_SIZE_PX, 0,
+    RINK_LENGTH_PX, CORNER_SEGMENT_SIZE_PX,
+    RINK_LENGTH_PX, RINK_WIDTH_PX - CORNER_SEGMENT_SIZE_PX,
+    RINK_LENGTH_PX - CORNER_SEGMENT_SIZE_PX, RINK_WIDTH_PX,
+    CORNER_SEGMENT_SIZE_PX, RINK_WIDTH_PX,
+    0, RINK_WIDTH_PX - CORNER_SEGMENT_SIZE_PX,
+    0, CORNER_SEGMENT_SIZE_PX,
+  );
+
+  return rinkBorder;
+}
+
+function getPlayer(): Sprite {
+  const player = Sprite.from('../../assets/images/jersey_red.png');
+  player.anchor.set(0.5);
+  player.x = RINK_LENGTH_PX / 2 - PLAYER_SIZE_PX * 2;
+  player.y = RINK_WIDTH_PX / 2;
+
+  return player;
+}
+
+function getPuck(): Graphics {
+  const puck = new Graphics();
+  puck.lineStyle(1, '#666');
+  puck.beginFill('#222');
+  puck.drawCircle(RINK_LENGTH_PX / 2, RINK_WIDTH_PX / 2, PUCK_RADIUS_PX);
+  puck.endFill();
+
+  return puck;
 }
